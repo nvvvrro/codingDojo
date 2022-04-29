2540057a9772e3bce56766d96c264227b8d22e6f
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-04-29 16:47:09 -0400

    fix: main and pitagoras

--- Java/java-fundamentals/fundamentals/javaFun/src/javaFun/Main.java
+++ Java/java-fundamentals/fundamentals/javaFun/src/javaFun/Main.java
@@ -3,13 +3,13 @@ package javaFun;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		String nombre="Marcelo";
 		int edad=24;
 		String ciudad="Santiago";
 		
 		System.out.println("Mi nombre es "+nombre+
-				"\nTengo "+edad+" años de edad."+ 
+				"\nTengo "+edad+" aï¿½os de edad."+ 
 				"\nMi ciudad es "+ciudad);
 	
 	}
--- Java/java-fundamentals/fundamentals/javaFun/src/pitagoras/PitagorasTest.java
+++ Java/java-fundamentals/fundamentals/javaFun/src/pitagoras/PitagorasTest.java
@@ -3,7 +3,7 @@ package pitagoras;
 public class PitagorasTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Pitagoras pitagoras = new Pitagoras();
 
 		double test = pitagoras.calcularHipotenusa(8, 3);
